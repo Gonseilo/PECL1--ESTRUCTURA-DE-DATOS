7b64ed55f5476642b7911cade1b0a6e75bf52b5b
ivan-lopezn <ivan.lopezn@edu.uah.es>
2022-10-30 11:13:48 +0100

    Optimización del main

--- PECL1--ESTRUCTURA DE DATOS/main.cpp
+++ PECL1--ESTRUCTURA DE DATOS/main.cpp
@@ -1,60 +1,35 @@
 #include <iostream>
-#include "stdio.h"
-#include <locale.h>
-#include "Paquetes.h"
 #include <conio.h>
+#include <string.h>
+#include <ctime>
+#include "Paquetes.h"
 #include "Colas.h"
 #include "Pilas.h"
 #include "Muelles.h"
-#include <string.h>
-#include <cstdlib>
-#include <ctime>
 
 using std::cout; using std::cin; using std::endl;
 
 Paquetes p;
 Muelles m;
-Pilas pi;
-int aux;
-
 Colas c1;
-Paquete *head1 = nullptr;
-Paquete *rear1 = nullptr;
-
 Colas c2;
-Paquete *head2 = nullptr;
-Paquete *rear2 = nullptr;
-
 Colas cNE;
-Paquete *headNE = nullptr;
-Paquete *rearNE = nullptr;
-char* NE = "NE";
-
 Colas cNO;
-Paquete *headNO = nullptr;
-Paquete *rearNO = nullptr;
-char* NO = "No";
-
 Colas cSE;
-Paquete *headSE = nullptr;
-Paquete *rearSE = nullptr;
-char* SE = "SE";
-
 Colas cSO;
-Paquete *headSO = nullptr;
-Paquete *rearSO = nullptr;
+Pilas piNE;
+Pilas piNO;
+Pilas piSE;
+Pilas piSO;
+
+char* NE = "NE";
+char* NO = "NO";
+char* SE = "SE";
 char* SO = "SO";
 
-Pilas piNE;
 Paquete* muelleNE[N2];
-
-Pilas piNO;
 Paquete* muelleNO[N2];
-
-Pilas piSE;
 Paquete* muelleSE[N2];
-
-Pilas piSO;
 Paquete* muelleSO[N2];
 
 int main(){
@@ -63,69 +38,34 @@ int main(){
 
     setlocale(LC_ALL, "spanish");
 
-    char* ptr;
-    Paquete* ptr2;
-
-    Paquete *tempPrint1 = nullptr;
-    c1.head = head1;
-    c1.rear = rear1;
-
-    Paquete *tempPrint2 = nullptr;
-    c2.head = head2;
-    c2.rear = rear2;
-
-    Paquete *tempPrintNE = nullptr;
-    cNE.head = headNE;
-    cNE.rear = rearNE;
-
-    Paquete *tempPrintNO = nullptr;
-    cNO.head = headNO;
-    cNO.rear = rearNO;
-
-    Paquete *tempPrintSE = nullptr;
-    cSE.head = headSE;
-    cSE.rear = rearSE;
-
-    Paquete *tempPrintSO = nullptr;
-    cSO.head = headSO;
-    cSO.rear = rearSO;
-
-    Paquete *tempaux;
+    Paquete* tempaux;
 
     for (int i=0;i<N1;i++){
         Paquete *temp = new Paquete();
 
-        ptr = p.GenerarID();
-        temp->informacion.codigoID = ptr;
-
-        ptr = p.GenerarLatitud();
-        temp->informacion.coordenadas.latitud = ptr;
-
-        ptr = p.GenerarLongitud();
-        temp->informacion.coordenadas.longitud = ptr;
-
-        ptr = p.GenerarDNI();
-        temp->informacion.DNI = ptr;
+        temp->informacion.codigoID = p.GenerarID();
+        temp->informacion.coordenadas.latitud = p.GenerarLatitud();
+        temp->informacion.coordenadas.longitud = p.GenerarLongitud();
+        temp->informacion.DNI = p.GenerarDNI();
 
         c1.Enqueue(temp);
     }
-    c1.Print(tempPrint1);
-    delete[] ptr;
+    c1.Print(tempaux);
 
     while ((c1.head != nullptr) && (c1.rear != nullptr)) {
         for (int i=0; i < N2; i++){
             c2.Enqueue(c1.Dequeue(tempaux));
         }
-        c2.Print(tempPrint2);
+        c2.Print(tempaux);
 
         for (int i = 0; i < N2; i++){
-            ptr2 = c2.Dequeue(tempaux);
-            char* muelle = m.AsignarMuelle(ptr2->informacion.coordenadas.latitud, ptr2->informacion.coordenadas.longitud);
+            tempaux = c2.Dequeue(tempaux);
+            char* muelle = m.AsignarMuelle(tempaux->informacion.coordenadas.latitud, tempaux->informacion.coordenadas.longitud);
 
             if (strcmp((muelle), "NO") == 0){
                 cout << "NO" << endl;
-                cNO.Enqueue(ptr2);
-                piNO.push(ptr2, muelleNO);
+                cNO.Enqueue(tempaux);
+                piNO.push(tempaux, muelleNO);
                 if (piNO.top == N3){
                     for (int i = 0; i < N3 ; i++){
                         piNO.pop(muelleNO);
@@ -135,8 +75,8 @@ int main(){
 
             if (strcmp((muelle), "NE") == 0){
                 cout << "NE" << endl;
-                cNE.Enqueue(ptr2);
-                piNE.push(ptr2, muelleNE);
+                cNE.Enqueue(tempaux);
+                piNE.push(tempaux, muelleNE);
                 if (piNE.top == N3){
                     for (int i = 0; i < N3 ; i++){
                         piNE.pop(muelleNE);
@@ -146,8 +86,8 @@ int main(){
 
             if (strcmp((muelle), "SO") == 0){
                 cout << "SO" << endl;
-                cSO.Enqueue(ptr2);
-                piSO.push(ptr2, muelleSO);
+                cSO.Enqueue(tempaux);
+                piSO.push(tempaux, muelleSO);
                 if (piSO.top == N3){
                     for (int i = 0; i < N3 ; i++){
                         piSO.pop(muelleSO);
@@ -157,8 +97,8 @@ int main(){
 
             if (strcmp((muelle), "SE") == 0){
                 cout << "SE" << endl;
-                cSE.Enqueue(ptr2);
-                piSE.push(ptr2, muelleSE);
+                cSE.Enqueue(tempaux);
+                piSE.push(tempaux, muelleSE);
                 if (piSE.top == N3){
                     for (int i = 0; i < N3 ; i++){
                         piSE.pop(muelleSE);
@@ -167,6 +107,7 @@ int main(){
             }
         }
 
+        int aux;
         cout << "Introduzca 1 para repetir el proceso: ";
         cin >> aux;
         if (aux == 1){
